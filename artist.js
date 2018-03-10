9f5fd7e401543875e5043ecc35a66b4dde6f1f45
Marco <mg27102@gmail.com>
2018-03-10 16:18:26 -0500

    javascript for artist sign up page

---
+++ artist.js
@@ -0,0 +1,52 @@
+ document.querySelector('.upload-image').addEventListener('change', uploadImage);
+
+  function uploadImage() {
+    var preview = document.querySelector('.image-preview');
+    var file = document.querySelector('.upload-image').files[0];
+    var reader = new FileReader();
+
+    reader.onloadend = function () {
+      var imageUrl = reader.result;
+      preview.src = imageUrl;
+      localStorage.imageUrl = imageUrl;
+    }
+
+    reader.readAsDataURL(file);
+  }
+
+  //
+
+document.querySelector('.submit').addEventListener('click', submitProfile);
+
+function submitProfile() {
+  var firstName = document.querySelector('.user-first-name').value;
+  var lastName = document.querySelector('.user-last-name').value;
+  var phoneNumber = document.querySelector('.phoneNumber').value;
+  var email = document.querySelector('.user-email').value;
+  var photoUrl = document.querySelector('.image-preview').value;
+  var portfolio = document.querySelector('.portfolio').value;
+  var preferLocation = document.querySelector('.location').value;
+
+
+  if (
+    firstName == '' ||
+    lastName == '' ||
+    phoneNumber == '' ||
+    email == '' ||
+    photoUrl == '' ||
+    portfolio == '' ||
+    preferLocation == ''
+  ) {
+    document.querySelector('.error').textContent = 'Please fill out all the fields!';
+  } else {
+    localStorage.firstName = firstName;
+    localStorage.lastName = lastName;
+    localStorage.phoneNumber = phoneNumber;
+    localStorage.email = email;
+    localStorage.photo = photoUrl;
+    localStorage.portfolio = portfolio;
+    localStorage.location = preferLocation;
+
+    location.href = 'artNotification.html';
+  }
+}
