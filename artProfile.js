b7e6529f7bfcb187f7165a91cd5e269554d0637d
Marco <mg27102@gmail.com>
2018-03-17 12:25:04 -0400

    Email appears as username AND contact info

--- artProfile.js
+++ artProfile.js
@@ -2,6 +2,8 @@ if (localStorage.length) {
   var fullName = localStorage.firstName + ' ' + localStorage.lastName;
   document.querySelector('.name').textContent = fullName;
   document.querySelector('.phoneNumber').textContent = localStorage.phoneNumber;
-  document.querySelector('.email').textContent = localStorage.email;
+  document.querySelectorAll('.email')[0].textContent = localStorage.email;
+  document.querySelectorAll('.email')[1].textContent = localStorage.email;
+
   document.querySelector('.location').textContent = localStorage.preferLocationlocation;
 }
