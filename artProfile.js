96bb3f37fbaaad0318eaebd98c6404ebc9396e81
Marco <mg27102@gmail.com>
2018-03-17 12:26:03 -0400

    random space

--- artProfile.js
+++ artProfile.js
@@ -4,6 +4,5 @@ if (localStorage.length) {
   document.querySelector('.phoneNumber').textContent = localStorage.phoneNumber;
   document.querySelectorAll('.email')[0].textContent = localStorage.email;
   document.querySelectorAll('.email')[1].textContent = localStorage.email;
-
   document.querySelector('.location').textContent = localStorage.preferLocationlocation;
 }
