54e42264f9a9f44744074ce758fa703ee125cbea
Marco <mg27102@gmail.com>
2018-03-18 14:41:53 -0400

    Improved  owner profile page

---
+++ ownProfile.js
@@ -0,0 +1,7 @@
+if (localStorage.length) {
+  document.querySelector('.name').textContent = localStorage.ownerName;
+  document.querySelector('.phoneNumber').textContent = localStorage.ownerPhoneNumber;
+  document.querySelectorAll('.email')[0].textContent = localStorage.ownerEmail;
+  document.querySelectorAll('.email')[1].textContent = localStorage.ownerEmail;
+  document.querySelector('.location').textContent = localStorage.ownerLocation;
+}
