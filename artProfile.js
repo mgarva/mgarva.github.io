a39f81d786b4fb735b8e09a9b2e86b4fc865173c
Marco <mg27102@gmail.com>
2018-03-23 21:01:59 -0400

    Made artist profile look better

--- artProfile.js
+++ artProfile.js
@@ -2,9 +2,9 @@ if (localStorage.length) {
   var fullName = localStorage.firstName + ' ' + localStorage.lastName;
   document.querySelector('.name').textContent = fullName;
   document.querySelector('.phoneNumber').textContent = localStorage.phoneNumber;
-  document.querySelectorAll('.email')[0].textContent = localStorage.email;
-  document.querySelectorAll('.email')[1].textContent = localStorage.email;
+  document.querySelector('.email').textContent = localStorage.email;
   document.querySelector('.location').textContent = localStorage.location;
   document.querySelector('#profile').src = localStorage.imageUrl;
   document.querySelector('.portfolio').href = localStorage.portfolio;
+  document.querySelector('.style').textContent = localStorage.style;
 }
