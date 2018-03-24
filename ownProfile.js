52f7759a72a1b0bcecfb8d93d7f880034f4620a9
Marco <mg27102@gmail.com>
2018-03-23 21:32:28 -0400

    Made owner profile page look better

--- ownProfile.js
+++ ownProfile.js
@@ -2,8 +2,7 @@ if (localStorage.length) {
   var fullName = localStorage.ownerFirstName + ' ' + localStorage.ownerLastName;
   document.querySelector('.name').textContent = fullName;
   document.querySelector('.phoneNumber').textContent = localStorage.ownerPhoneNumber;
-  document.querySelectorAll('.email')[0].textContent = localStorage.ownerEmail;
-  document.querySelectorAll('.email')[1].textContent = localStorage.ownerEmail;
+  document.querySelector('.email').textContent = localStorage.ownerEmail;
   document.querySelector('.location').textContent = localStorage.ownerLocation;
   document.querySelector('.wallImg').src = localStorage.ownerImageUrl;
 }
